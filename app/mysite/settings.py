e4e1024c80e1b266d9216389b4aa945c46bfe81b
dima_tolshin <dima_tolshin@mail.ru>
2025-03-26 14:25:38 +0300

    test

--- app/mysite/settings.py
+++ app/mysite/settings.py
@@ -91,7 +91,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=10),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
