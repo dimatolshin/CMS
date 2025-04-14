67cc9d6b3f28d5e99aeab73dd8eea765264f2c50
dima_tolshin <dima_tolshin@mail.ru>
2025-04-14 18:16:54 +0300

    fix

--- app/main/views.py
+++ app/main/views.py
@@ -38,7 +38,6 @@ class LoginView(TokenObtainPairView):
             key='refresh_token',
             value=refresh_token,
             max_age=timedelta(days=1),
-            domain='gang-soft.com',
             httponly=False,
             secure=True,  # Только для HTTPS
             samesite='Strict',  # Защита от CSRF
@@ -47,7 +46,6 @@ class LoginView(TokenObtainPairView):
             key='access_token',
             value=access_token,
             max_age=timedelta(days=10),
-            domain='gang-soft.com',
             httponly=False,
             secure=True,  # Только для HTTPS
             samesite='Strict',  # Защита от CSRF
